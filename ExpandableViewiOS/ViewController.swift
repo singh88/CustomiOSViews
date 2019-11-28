90c3ddbcdf3beb78a567bb2d07cee417871799be
Manish Singh <manish.singh88@gmail.com>
2019-11-27 17:59:51 -0800

    first commit

---
+++ ExpandableViewiOS/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  GetStrong
+//
+//  Created by Manish Singh on 11/20/19.
+//  Copyright © 2019 ManishSingh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var firstView: UIView!
+    @IBOutlet var secondView: UIView!
+    @IBOutlet var mainHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var showMoreButton: UIButton!
+
+    private var shouldCollapse = false
+
+    var buttonTitle: String {
+        return shouldCollapse ? "Show Less" : "Show More"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        animateView(isCollapse: false, buttonText: buttonTitle, heighConstraint: 0)
+    }
+
+    @IBAction func buttonTap(_ sender: Any) {
+        if shouldCollapse {
+            animateView(isCollapse: false,
+                        buttonText: buttonTitle,
+                        heighConstraint: 0)
+        } else {
+            animateView(isCollapse: true,
+                        buttonText: buttonTitle,
+                        heighConstraint: 100)
+        }
+    }
+
+    private func animateView(isCollapse: Bool,
+                             buttonText: String,
+                             heighConstraint: Double) {
+        shouldCollapse = isCollapse
+        mainHeightConstraint.constant = CGFloat(heighConstraint)
+        showMoreButton.setTitle(buttonTitle, for: .normal)
+        UIView.animate(withDuration: 1) {
+            self.view.layoutIfNeeded()
+        }
+    }
+}
